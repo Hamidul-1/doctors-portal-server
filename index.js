a9eadf3c77f7e0aa9be9be52417b68cb2ffc6c36
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-14 14:56:33 +0600

    first commit

--- index.js
+++ index.js
@@ -10,14 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.yr9x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yr9x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello From doctor Uncle!!')

3a6166708a36f40ca8ddb3cea17d1f79f5215a9e
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-14 14:10:11 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const packageName = require('cors');
+const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://doctor_admin:<password>@cluster0.yr9x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello From doctor Uncle!!')
